c221d9b38c116d84c1a418a8463c8118a06ea90c
Yang <yahai@ucsc.edu>
2020-06-23 10:50:47 -0700

    过于简单

--- 58.最后一个单词的长度.py
+++ 58.最后一个单词的长度.py
@@ -7,5 +7,10 @@
 # @lc code=start
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+
+        words = s.split()
+
+        return len(words[-1]) if words else 0
+        
 # @lc code=end
 
